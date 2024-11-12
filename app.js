de302d60a1b4d8e400daed8f0e9a2888f282baa1
Eric Onah <ericdavid4u@gmail.com>
2024-11-12 08:38:26 +0100

    added days of the week

--- app.js
+++ app.js
@@ -3,21 +3,34 @@ import { fileURLToPath } from "url";
 import { dirname } from "path";
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
-// use express to handle form data and JSON requests(request body)
+// set EJS as template engine
+
+app.set("view engine", "ejs");
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// defining __dirname to extract directory path
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// use express to render statics from the root folder
-app.use(express.static(__dirname));
+// app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "index.html");
-});
+  //
 
-app.listen(port, () => console.log(`Server started at port ${port}!`));
+  const dayName = {
+    0: "Sunday",
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+  };
+
+  let day = new Date().getDay();
+  let today = dayName[day];
+  res.render("list", { kindOfDay: today });
+});
+app.listen(port, () => console.log("Server started!"));
